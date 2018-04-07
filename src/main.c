920b302dc89b06799c872f725163d03b4bbec95c
Kawaharasouta <kawahara6514@gmail.com>
2018-04-07 19:23:04 +0900

    pcap header & packet header

--- src/main.c
+++ src/main.c
@@ -2,7 +2,13 @@
 #include<stdlib.h>
 #include<string.h>
 
+#include<pcap.h>
+
+#include<sys/time.h>
+
 #include"initrawsock.h"
+
+void print_0xpcaphdr(struct pcap_file_header *pcap_hdr);
 
 int main(int argc, char **argv, char *envp[]){
 	int sock, size;
@@ -20,20 +26,45 @@ int main(int argc, char **argv, char *envp[]){
 
 	//printf("sock: %d\n",sock);
 
+	struct pcap_file_header pcap_hdr;
+	struct pcap_pkthdr pkt_hdr;
+
+	/*struct timeval tv;
+	struct timezone tz;
+	gettimeofday(&tv, &tz);*/
+
+	pcap_hdr.magic = (bpf_int32)0xa1b2c3d4;
+	//pcap_hdr.magic = TCPDUMP_MAGIC;???
+	pcap_hdr.version_major = PCAP_VERSION_MAJOR;
+	pcap_hdr.version_minor = PCAP_VERSION_MINOR;
+	pcap_hdr.thiszone = 0;
+	pcap_hdr.sigfigs = 0;
+	pcap_hdr.snaplen = 0x00040000;
+	pcap_hdr.linktype = 0x00000001;
+
+	//gettimeofday(&(pkt_hdr.ts), 0);
+
+	FILE *fp;
+	fp = fopen("test.pcap", "wb");
+
 	while (1){
 		if (((size = read(sock, buf, sizeof(buf))) <= 0)){
 			perror("read");
-			printf("read 0\n");
-
 		}
 		else{
-#if 0
-			printf("size: %d\n",size);
-			hexdump(buf, strlen(buf));
+#if 1
+			printf("***\n");
+			gettimeofday(&(pkt_hdr.ts), 0);
+			pkt_hdr.caplen = size;
+			pkt_hdr.len = size;
+
+			print_0xpcaphdr(&pcap_hdr);
 #else
 			printf("size: %d\n",size);
 			hexdump(buf, size);
 #endif
+
+			fclose(fp);
 		}
 	}
 }
@@ -48,4 +79,18 @@ void hexdump(u_int16_t *buf, int size){
 		}
 	}
 	fprintf(stdout, "\nfin\n");
+}
+
+void output_pcaphdr(struct pcap_file_header *pcap_hdr, FILE *fp){
+	
+}
+
+void print_0xpcaphdr(struct pcap_file_header *pcap_hdr){
+	printf("%08x\n", htonl(pcap_hdr->magic));
+	printf("%04x\n", htons(pcap_hdr->version_major));
+	printf("%04x\n", htons(pcap_hdr->version_minor));
+	printf("%08x\n", htonl(pcap_hdr->thiszone));
+	printf("%08x\n", htonl(pcap_hdr->sigfigs));
+	printf("%08x\n", htonl(pcap_hdr->snaplen));
+	printf("%08x\n", htonl(pcap_hdr->linktype));
 }
