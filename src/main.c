083ea840df744496c5c05b19e86a6d1449009c4f
Kawaharasouta <kawahara6514@gmail.com>
2018-04-07 15:02:37 +0900

    save 04.06

--- src/main.c
+++ src/main.c
@@ -4,28 +4,34 @@
 
 #include"initrawsock.h"
 
-int main(int argc, char **argv){
+int main(int argc, char **argv, char *envp[]){
 	int sock, size;
-	char buf[2048];
+	u_char buf[2048];
 
 	if (argc < 2){
 		fprintf(stderr, "usage: ./main [dev-name]\n");
 		exit(1);
 	}
 
-	if (sock = initrawsock(argv[1], 1, 0) < 0){
+	if ((sock = initrawsock(argv[1], 0, 0)) < 0){
 		fprintf(stderr, "InitRawSocket:error:%s\n", argv[1]);
 		exit(1);
 	}
 
+	//printf("sock: %d\n",sock);
+
 	while (1){
-		if ((size = read(sock, buf, sizeof(buf)) < 0)){
+		if (((size = read(sock, buf, sizeof(buf))) <= 0)){
 			perror("read");
+			printf("read 0\n");
+
 		}
 		else{
 #if 0
+			printf("size: %d\n",size);
 			hexdump(buf, strlen(buf));
 #else
+			printf("size: %d\n",size);
 			hexdump(buf, size);
 #endif
 		}
